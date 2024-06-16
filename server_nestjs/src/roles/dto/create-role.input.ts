f7985233ada99042cfa960d859881a911b077af8
Roman <romanroketskiy05@gmail.com>
2024-06-16 16:47:53 +0300

    updateee

--- server_nestjs/src/roles/dto/create-role.input.ts
+++
@@ -1,9 +0,0 @@
-import { InputType, Field } from '@nestjs/graphql';
-import { IsString } from 'class-validator';
-
-@InputType()
-export class CreateRoleInput {
-  @Field()
-  @IsString()
-  role_name: string; // Define the role_name field
-}
